c5489be59e3b95cea08b23c327c60be3ab14b415
Akihiro Moritomo <moooti190@gmail.com>
2018-11-12 22:57:10 +0900

    ファンクショナルコンポーネントを導入した

--- src/index.js
+++ src/index.js
@@ -2,16 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-//stateを持っていないためコンストラクタを消した。
-  render() {
-    return (
-      //buttonクリックすると、親からの（boare）onClickが呼び出される
-      <button className="square"　onClick={()=>this.props.onClick()}>
-        {this.props.value}
-      </button>
-    );
-  }
+// class Square extends React.Component {
+// //stateを持っていないためコンストラクタを消した。
+//   render() {
+//     return (
+//       //buttonクリックすると、親からの（boare）onClickが呼び出される
+//       <button className="square"　onClick={()=>this.props.onClick()}>
+//         {this.props.value}
+//       </button>
+//     );
+//   }
+// }
+
+function Square(props){
+  return(
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
 
 class Board extends React.Component {
